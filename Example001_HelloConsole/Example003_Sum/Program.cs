bede6b4ce558325697b30052cdb58627ced7eabc
Violetta <forbes.life@mail.ru>
2022-04-10 15:27:37 +0300

    Example005

--- Example001_HelloConsole/Example003_Sum/Program.cs
+++ Example001_HelloConsole/Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 5;
-int numberB = 7;
+﻿int numberA = new Random().Next(1,10);
+Console.Write(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write(numberB);
 int result = numberA + numberB;
 Console.WriteLine (result);
